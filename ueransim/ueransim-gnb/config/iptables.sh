a1cd014a8c246c21c140d5581e032e6c29daa60a
edierbra <edierbra@unicauca.edu.co>
2024-06-20 10:44:02 -0400

    Adding lb

--- ueransim/ueransim-gnb/config/iptables.sh
+++ ueransim/ueransim-gnb/config/iptables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ### Implement networking rules
-ip route add 20.20.20.1/32 dev lb via 10.10.2.1 && sleep infinity
+ip route add 20.20.20.1 via 10.10.2.1
+sleep infinity
--- ueransim/ueransim-gnb/config/wrapper.sh
+++ ueransim/ueransim-gnb/config/wrapper.sh
@@ -2,4 +2,5 @@
     /ueransim/nr-gnb --config /ueransim/config/free5gc-gnb.yaml
 
     ### Implement networking rules
-    ip route add 20.20.20.1/32 dev lb via 10.10.2.1 && sleep infinity
+    ip route add 20.20.20.1 via 10.10.2.1
+    sleep infinity
