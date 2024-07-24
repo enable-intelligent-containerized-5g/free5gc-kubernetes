ea452a0d50626fb2531446cd7ccc03c4ec01eb2f
edierbravo <edierbra@unicauca.edu.co>
2024-07-23 23:54:32 -0500

    Changes

--- ueransim/ueransim-gnb/config/wrapper.sh
+++ ueransim/ueransim-gnb/config/wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
-    /ueransim/nr-gnb --config /ueransim/config/free5gc-gnb.yaml
 
     ### Implement networking rules
-    ip route add 20.20.20.1 via 10.10.2.1
+    ip route add 20.20.20.1/32 dev n2 via 10.10.2.1
     sleep infinity
+
+    ### run gnb
+    /ueransim/nr-gnb --config /ueransim/config/free5gc-gnb.yaml
+
+    
