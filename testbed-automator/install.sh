94ba9319236fb2dae82a279c470df4405a70365f
edierbravo <edierbra@unicauca.edu.co>
2024-07-24 21:12:17 -0500

    Changes

--- testbed-automator/install.sh
+++ testbed-automator/install.sh
@@ -81,13 +81,10 @@ install-containerd() {
 		sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 		# Add the repository to Apt sources:
-		echo \
-		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		
 		sudo apt-get update
-		sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin		sudo mkdir -p /etc/containerd
+		sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 		sudo bash -c 'containerd config default > /etc/containerd/config.toml'
 		sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 		sudo systemctl enable containerd
