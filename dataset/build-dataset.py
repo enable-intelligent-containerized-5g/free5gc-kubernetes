502a15fd08899111234af69d167e046d15d818fc
edierbravo <edierbra@unicauca.edu.co>
2024-08-29 02:18:48 -0500

    Add dataset scripts

--- dataset/build-dataset.py
+++ dataset/build-dataset.py
@@ -24,7 +24,7 @@ def update_dataset(df1, df2, file_name):
     df2.update(df1[rows_existing])
     df2 = pd.concat([df2, df1[rows_missing]], ignore_index=False)
 
-    print(f"Success: File '{file_name}' added.\n")
+    print(f"  Success: File '{file_name}' added.\n")
     return df2
     
 def yes_no_question(input_ms, yes_ms, not_ms):
@@ -46,37 +46,37 @@ def duplicated_indexes(data_frame):
 
 def main():
     # Create the parser
-    parcer = argparse.ArgumentParser(description='Descripción de tu script.')
+    parser = argparse.ArgumentParser(description='Build the dataset.')
 
     # Add args
     default_output = "dataset.csv"
     default_data = "data/"
-    parcer.add_argument('-o', '--output', dest='output', type=str, required=False, 
-                        help=f'Dataset directory (default is "{default_output}").', 
+    parser.add_argument('-o', '--output', dest='output', type=str, required=False, 
+                        help=f"Dataset directory, default is '{default_output}'.", 
                         default=default_output)
-    parcer.add_argument('-d', '--data', dest='data', type=str, required=False, 
-                        help=f'Data directory (default is "{default_data}").', 
+    parser.add_argument('-d', '--data', dest='data', type=str, required=False, 
+                        help=f"Data directory, default is '{default_data}'.", 
                         default=default_data)
 
-    # Parsea los argumentos
-    args = parcer.parse_args()
-    args_output = args.output
-    args_data = args.data.rstrip('/')
+    # Parse the args
+    args = parser.parse_args()
+    arg_output = args.output
+    arg_data = args.data.rstrip('/')
 
-    if not exist_dataset(args_output):
+    if not exist_dataset(arg_output):
         # Create the CSV file and write the head row
-        with open(args_output, mode='w', newline='') as file:
+        with open(arg_output, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(columns)
-        print(f"File '{args_output}' created.\n")
+        print(f"File '{arg_output}' created.\n")
     else:
-        print(f"The dataset '{args_output}' will be overwritten.\n")
+        print(f"The dataset '{arg_output}' will be overwritten.\n")
 
-    df_final = pd.read_csv(args_output)
+    df_final = pd.read_csv(arg_output)
     df_final.set_index(indexes)
 
-    # Filter only the CSV files.
-    files = os.listdir(args_data)
+    # Filter only the JSON files.
+    files = os.listdir(arg_data)
     files = [f for f in files if f.endswith('.json')]
 
     # Read each JSON file and process the data.
@@ -85,28 +85,28 @@ def main():
         base_filename = os.path.splitext(filename)[0]
         parts_filename = base_filename.split('_')
 
-        if len(parts_filename) == 4:
+        if len(parts_filename) == 5:
             if filename.endswith('.json'):
-                # Parsear el nombre del archivo para extraer el namespace y la métrica
-                namespace, metric, date, time = filename.split('_')
+                # Parse the filename and extract the namespace and the metric
+                namespace, metric = filename.split('_')[:2]
 
                 # Add metric column
                 if metric not in df_final.columns:
                     df_final[metric] = pd.NA
                 
-                # Leer el archivo JSON
-                with open(os.path.join(args_data, filename), 'r') as file:
+                # Read the JSON file
+                with open(os.path.join(arg_data, filename), 'r') as file:
                     json_data = json.load(file)
                 
-                # Verificar que el JSON tiene un estado de éxito
-                if json_data.get('status') == 'success':
-                    # Extraer los resultados
+                # Verify the JSON data
+                if (json_data.get('status') == 'success') and json_data['data']['result']:
+                    # Extract the result
                     results = json_data['data']['result']
                     
-                    # Crear una lista de filas para el DataFrame temporal
+                    """List for temporal DataFrame"""
                     rows = []
                     
-                    # Procesar cada resultado para extraer el pod, container, tiempo y valor
+                    """Extract the pod, container, time, and value"""
                     for result in results:
                         metric_info = result['metric']
                         if 'container' in metric_info:
@@ -114,10 +114,10 @@ def main():
                             container = metric_info['container']
                             values = result['values']
                             
-                            # Agregar cada valor a la lista de filas
+                            """Add value to rows"""
                             for value in values:
                                 timestamp, metric_value = value
-                                # Crear una fila en el DataFrame temporal
+                                """Create a row in the temporal DataFrame"""
                                 rows.append({
                                     'time': timestamp,
                                     'namespace': namespace,
@@ -132,19 +132,18 @@ def main():
                     # Set the index to temporal DataFrame
                     temp_df.set_index(indexes)
 
-                    # OPTION 1
                     # Duplicated indexes
                     duplicated_indexes_temp_df = duplicated_indexes(temp_df)
                     duplicated_temp_df = temp_df.index[duplicated_indexes_temp_df]
                     num_duplicate_indexes_temp_df = len(duplicated_temp_df)
                     
                     if temp_df.empty:
-                        print(f"Error: The file '{filename}' is empty.\n")
+                        print(f"  Error: The file '{filename}' is empty.\n")
 
                     elif duplicated_indexes_temp_df.any():
-                        print(f"Error: {num_duplicate_indexes_temp_df} Duplicated indexes found in '{filename}' \n-> {duplicated_temp_df}")
+                        print(f"  Error: {num_duplicate_indexes_temp_df} Duplicated indexes found in '{filename}' \n-> {duplicated_temp_df}")
                         removing_idx_dp = yes_no_question(
-                            f"Do you want to remove duplicate indices from '{filename}' and add them to the dataset? (y/n): ", 
+                            f"  Do you want to remove duplicate indices from '{filename}' and add them to the dataset? (y/n): ", 
                             "Removing duplicate indices", 
                             f"File '{filename}' discarded")
                         if removing_idx_dp:
@@ -153,15 +152,17 @@ def main():
 
                             # Save the temp_df
                             temp_df = temp_df.reset_index()
-                            temp_df.to_csv(args_data + "/" + filename, index=False)
+                            temp_df.to_csv(arg_data + "/" + filename, index=False)
                         else:
                             print()
-                    
                     else:
                         df_final = update_dataset(temp_df, df_final, filename)
-                        
+                else:
+                    print(f"  Error: The file '{filename}' haven't data or is empty.\n")
+            else:
+                print(f"  Error: Name file '{filename}' isn't JSON file.\n")
         else:
-            print(f"Error: Name file '{filename}' unexpected. Required name: 'namespace_metric_date_time.json'.\n")
+            print(f"  Error: Name file '{filename}' unexpected. Required name: 'namespace_metric_date_time.json'.\n")
 
     # Reset the column indexes
     df_final = df_final.reset_index(drop=True)
@@ -171,8 +172,8 @@ def main():
     df_final = df_final[~df_final.astype(str).apply(lambda x: x.str.contains('undefined', na=False)).any(axis=1)]
     
     # Save the dataset like a CSV file
-    df_final.to_csv(args_output, index=False)
-    print(f"The file '{args_output}' has been updated.")
+    df_final.to_csv(arg_output, index=False)
+    print(f"The file '{arg_output}' has been updated.")
 
 if __name__ == '__main__':
     main()
---
+++ dataset/get-data.py
@@ -0,0 +1,171 @@
+import re
+import json
+import argparse
+import requests
+import subprocess
+from datetime import datetime, timezone, timedelta
+
+data_path = "data/"
+
+"""Time"""
+timestamp = end_time = datetime.now()
+
+def is_valid_duration(duration):
+    # Regular expression pattern to match numbers followed by 'h' or 'm'
+    pattern = r'^\d+[hm]$'
+    return bool(re.match(pattern, duration))
+
+def parse_duration(duration_str):
+    """Convert a duration string (e.g. '1h', '30m') to seconds."""
+    if is_valid_duration(duration_str):
+        unit = duration_str[-1].lower()
+        value = int(duration_str[:-1])
+        if unit == 'h':
+            return value * 3600
+        elif unit == 'm':
+            return value * 60
+        else:
+            raise ValueError(
+                f"Invalid duration format: '{duration_str}'. Use 'h' for hours or 'm' for minutes.")
+    else:
+        raise ValueError(
+                f"Invalid duration format: '{duration_str}'. Use 'h' for hours or 'm' for minutes.")
+
+
+def get_epoch_range(duration, end_time):
+    """Calculate the time range in epoch based on the duration"""
+    # end_time = datetime.now(timezone.utc)
+    start_time = end_time - timedelta(seconds=duration)
+    return int(start_time.timestamp()), int(end_time.timestamp())
+
+
+def generate_filename(namespace, resource, duration, timestamp):
+    """To generate a filename based on a namespace and a resource"""
+    return f"{namespace}_{resource}_{duration}_{timestamp}.json"
+
+
+def fetch_and_save_query(base_url, params, query_info, namespace, duration, timestamp):
+    """Get and save the results in a JSON file"""
+    print(f"\nResource: '{query_info['resource']}', Unit: '{query_info['unit']}', Namespace '{namespace}'")
+    print(f"  Range ({duration}): from '{datetime.fromtimestamp(int(params['start'])).strftime('%Y-%m-%d %H:%M:%S')}'",
+        f"to '{datetime.fromtimestamp(int(params['end'])).strftime('%Y-%m-%d %H:%M:%S')}'")
+    params['query'] = query_info['query'].format(namespace=namespace)
+    response = requests.get(base_url, params=params)
+
+    print(
+        f"  Status code: {response.status_code}")
+
+    # timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_file = data_path + generate_filename(
+        namespace, query_info['resource'], duration, timestamp.strftime("%Y-%m-%d_%H-%M-%S"))
+
+    if response.headers.get('Content-Type', '').startswith('application/json'):
+        data = response.json()
+        with open(output_file, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"    Data saved in '{output_file}'")
+    else:
+        print(
+            f"    Response for '{query_info['resource']}' in '{namespace}' isn't JSON. Saving text response.")
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(response.text)
+        print(f"    Response save in '{output_file}'")
+
+def main():
+    """Set the parser"""
+    default_namespace = "free5gc"
+    default_duration = "1h"
+    parser = argparse.ArgumentParser(
+        description='Get the pods and containers of a namespace.')
+    parser.add_argument('-n', '--namespace', dest='namespace', type=str, required=False, 
+                        help=f'Namespace name (default is "{default_namespace}").', 
+                        default=default_namespace)
+    parser.add_argument('-t', '--duration', dest='duration', type=str, required=False, 
+                        help=f"Duration in minutes or hours (e.g., '1h' for one hour, '30m' for 30 minutes). Default is '{default_duration}').", 
+                        default=default_duration)
+
+    """Parse the args"""
+    args = parser.parse_args()
+    arg_namespace = args.namespace
+    arg_duration = args.duration
+
+    """Run the kubectl comand and get the output in a JSON format"""
+    result = subprocess.run(['kubectl', 'get', 'pods', '-n',
+                            arg_namespace, '-o', 'json'], capture_output=True, text=True)
+
+    """Verify if the command was successful."""
+    if result.returncode == 0:
+        """Load the JSON in a dictionay of Python"""
+        pods_json = json.loads(result.stdout)
+
+        """Create a dictionary to save the pod and container names."""
+        pods_containers = {}
+        for pod in pods_json['items']:
+            pod_name = pod['metadata']['name']
+            containers = [container['name']
+                        for container in pod['spec']['containers']]
+            pods_containers[pod_name] = containers
+
+        """Create a dictionary and store the list of pod and container names under the key pods"""
+        pods_dict = {"pods": pods_containers}
+
+        """print the dictionay in JSON format"""
+        # print(f"Pods: {json.dumps(pods_dict, indent=4)}")
+    else:
+        print(f"Error running kubectl comand: {result.stderr}")
+
+    """Structure queries"""
+    namespaces = {
+        arg_namespace: {
+            "pods": pods_dict["pods"],
+            "queries": [
+                {
+                    "resource": "memory-usage",
+                    "query": 'sum(container_memory_usage_bytes{{namespace="{namespace}", container!~".*wait-.*"}}) by (pod, container)',
+                    "unit": "bytes"
+                },
+                {
+                    "resource": "cpu-usage",
+                    "query": 'sum(rate(container_cpu_usage_seconds_total{{namespace="{namespace}", container!~"wait-.*"}}[1m])) by (pod, container)',
+                    "unit": "cores"
+                },
+                {
+                    "resource": "cpu-limit",
+                    "query": 'avg(kube_pod_container_resource_limits{{namespace="{namespace}", container!~"wait-.*", unit="core"}}) by (pod, container)',
+                    "unit": "cores"
+                }
+            ]
+        }
+        # You can add more namespaces here if necessary.
+    }
+
+    try:
+        duration_seconds = parse_duration(arg_duration)
+        start_epoch, end_epoch = get_epoch_range(duration_seconds, timestamp)
+    except ValueError as e:
+        print(f"Error: {e}")
+        exit(1)
+
+    """URL base y and params"""
+    base_url = "http://localhost:30090/api/v1/query_range"
+    params = {
+        "start": str(start_epoch),
+        "end": str(end_epoch),
+        "step": "14"
+    }
+
+    """Make the queries and save the results"""
+    for namespace, info in namespaces.items():
+        print(f"Processing namespace: {namespace}")
+        print(f"Pods in this namespace:")
+        for pod_name, pod_values in info['pods'].items():
+            print(f"  {pod_name}: {', '.join(pod_values)}")
+
+        for query_info in info['queries']:
+            fetch_and_save_query(base_url, params, query_info, namespace, arg_duration, timestamp)
+
+    print(
+        f"\nTime range used: From '{datetime.fromtimestamp(start_epoch, timezone.utc)}' to '{datetime.fromtimestamp(end_epoch, timezone.utc)}'")
+    
+if __name__ == '__main__':
+    main()
