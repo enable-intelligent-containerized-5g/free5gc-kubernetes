622bb067a8b1fa71f5b7903b38c91ed729f389e2
edierbravo <edierbra@unicauca.edu.co>
2024-11-30 20:39:46 -0500

    Fix: [error] TUN allocation failure [Open failure /dev/net/tun]

--- ueransim/ueransim-ue/automator-ue132/wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir /dev/net
-mknod /dev/net/tun c 10 200
-
-# nohup /ueransim/config/ue-iptables.sh &
-# nohup /ueransim/nr-ue -c /ueransim/config/ue.yaml
-
-/ueransim/nr-ue -c /ueransim/config/ue.yaml
--- ueransim/ueransim-ue/ue-automator/base/wrapper.sh
+++ ueransim/ueransim-ue/ue-automator/base/wrapper.sh
@@ -2,6 +2,7 @@
 
 mkdir /dev/net
 mknod /dev/net/tun c 10 200
+# chmod 755 /dev/net/tun
 
 # nohup /ueransim/config/ue-iptables.sh &
 # nohup /ueransim/nr-ue -c /ueransim/config/ue.yaml
